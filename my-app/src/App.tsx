d5d04df5f4d372df284b31c9644bb1061f0a248e
Benny <bendelrosario14@gmail.com>
2022-07-25 16:16:28 +1200

    added the mui text bar and the submit button

--- my-app/src/App.tsx
+++ my-app/src/App.tsx
@@ -1,17 +1,48 @@
 import React from 'react';
 import './App.css';
 import { useState } from "react";
+import Box from '@mui/material/Box';
+import TextField from '@mui/material/TextField';
+import Button from '@mui/material/Button';
 
 
 function App() {
-  return (
-    <div className="Homepage">
 
-      <div>
-      <p>
-        Hi i'm a recipe
-      </p>
+  const [Input, setInput] = useState("");
+
+
+  function search(){
+    alert("LOOKING FOOR FOOD")
+  }
+
+
+  return (
+
+
+    <div className="Homepage">
+      <h1>Recipe Renovator</h1>
+
+
+      <div className="Searchbar">
+        <Box
+          component="form"
+          sx={{
+            '& > :not(style)': { m: 1, width: '25ch' },
+          }}
+          noValidate
+          autoComplete="off"
+        >
+          <TextField id="outlined-basic" label="Food" variant="outlined" />
+          <Button variant="outlined">Submit</Button>
+        </Box>
+          
       </div>
+
+      <div className="Recipes">
+
+      </div>
+
+
     </div>
   );
 }
