008891175e39218fd384fcf92fe8cbfa2cc7d15d
Benny <bendelrosario14@gmail.com>
2022-07-25 17:01:16 +1200

    Got axios call working

--- my-app/src/App.tsx
+++ my-app/src/App.tsx
@@ -1,4 +1,5 @@
 import React from 'react';
+import axios from "axios";
 import './App.css';
 import { useState } from "react";
 import Box from '@mui/material/Box';
@@ -8,11 +9,17 @@ import Button from '@mui/material/Button';
 
 function App() {
 
+
+  // State variable for the search input.
   const [Input, setInput] = useState("");
 
 
-  function search(){
-    alert("LOOKING FOOR FOOD")
+  const RECIPE_BASE_URL = "https://api.spoonacular.com/recipes/complexSearch?";
+  const KEY_URL = "apiKey=cb1c464d94f142c08b156c5beddade8b";
+  function search() {
+    axios.get(RECIPE_BASE_URL + KEY_URL + "&query=" + Input).then((res) => {
+      console.log(res.data);
+    });
   }
 
 
@@ -20,7 +27,7 @@ function App() {
 
 
     <div className="Homepage">
-      <h1>Recipe Renovator</h1>
+      <h1>Recipe Renovator 2.0</h1>
 
 
       <div className="Searchbar">
@@ -32,14 +39,31 @@ function App() {
           noValidate
           autoComplete="off"
         >
-          <TextField id="outlined-basic" label="Food" variant="outlined" />
-          <Button variant="outlined">Submit</Button>
+          <TextField
+            id="outlined-basic"
+            label="Food"
+            variant="outlined"
+            onChange={e => setInput(e.target.value)}
+            onKeyPress={e => {
+              if (e.key === 'Enter') {
+                search()
+              }
+            }}
+          />
+          <Button variant="outlined"
+            onClick={e => { search() }}
+          >Submit</Button>
         </Box>
-          
+
       </div>
 
-      <div className="Recipes">
+      <div>
+        <p>you searched for {Input}</p>
 
+        <div id="recipe-result">
+          These will be the recipe results.
+
+        </div>
       </div>
 
 
